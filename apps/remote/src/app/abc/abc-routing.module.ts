93a76cda5f33a1577f32b84bf2283a45f90a0ee1
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-03 09:47:37 -0600

    configure routes

--- apps/remote/src/app/abc/abc-routing.module.ts
+++ apps/remote/src/app/abc/abc-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbcComponent } from './abc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AbcComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
