36a97b7cc7be9ede90a8d2cd9fb76c337f7ae83b
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-03 09:28:55 -0600

    add @angular-architects/module-federation for remote and host apps

---
+++ apps/remote/webpack.config.js
@@ -0,0 +1,56 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.base.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "remote",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        // name: "remote",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './apps/remote/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "firstMicrofrontend": "http://localhost:0/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
