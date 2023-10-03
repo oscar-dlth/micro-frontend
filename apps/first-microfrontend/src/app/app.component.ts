148894a678fecbfe1d076755acb2c30b4fa4d55e
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-03 09:14:40 -0600

    Initial commit

---
+++ apps/first-microfrontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-microfrontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'first-microfrontend';
+}
