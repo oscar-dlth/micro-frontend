4cc60302555ad411a137eb00d3734d78e6c8107a
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-03 09:31:48 -0600

    add ABC module

---
+++ apps/remote/src/app/abc/abc.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AbcRoutingModule } from './abc-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AbcRoutingModule
+  ]
+})
+export class AbcModule { }
