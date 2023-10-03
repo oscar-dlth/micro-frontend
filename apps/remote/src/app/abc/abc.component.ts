6a5c69e499c0f26a68b33f0b77069c780fce0dee
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-03 09:35:27 -0600

    add abc component

---
+++ apps/remote/src/app/abc/abc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-microfrontend-abc',
+  templateUrl: './abc.component.html',
+  styleUrls: ['./abc.component.css']
+})
+export class AbcComponent {
+
+}
--- apps/remote/src/app/abc/abc.module.ts
+++ apps/remote/src/app/abc/abc.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AbcRoutingModule } from './abc-routing.module';
+import { AbcComponent } from './abc.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AbcComponent
+  ],
   imports: [
     CommonModule,
     AbcRoutingModule
